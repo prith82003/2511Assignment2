bef0aaf1e2a3731fc3a5e0fc7f6370af7ea0e8f9
Aryan Singh <aryan.singh@student.unsw.edu.au>
2023-11-03 22:14:31 +1100

    Task1d implemented interface

--- src/main/java/dungeonmania/entities/collectables/Collectable.java
+++ src/main/java/dungeonmania/entities/collectables/Collectable.java
@@ -1,12 +1,13 @@
 package dungeonmania.entities.collectables;
 
 import dungeonmania.entities.Entity;
+import dungeonmania.entities.IOverlappable;
 import dungeonmania.entities.inventory.InventoryItem;
 import dungeonmania.util.Position;
 import dungeonmania.entities.Player;
 import dungeonmania.map.GameMap;
 
-public abstract class Collectable extends Entity implements InventoryItem {
+public abstract class Collectable extends Entity implements InventoryItem, IOverlappable {
     public Collectable(Position position) {
         super(position);
     }
